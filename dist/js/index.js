f4eff22f0e5b60b5bcb03c975354c7e08b378c9c
feng-p <fengpu_ovo@163.com>
2020-08-31 22:17:49 +0800

    monday

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,10 +1,67 @@
 //轮播
 $(function() {
-    //轮播的数据
-    var sliderArr = ["../img/banner1.jpg", "../img/banner1.jpg"];
-    $(".banner-img-box img").attr("src", arr[0]);
-    var sliderIndex = 0;
-    var timer = null;
-    //点击按钮时切换图片
-    $(".banner-img-box")
+        var num = 0;
+        slider(); //确保页面加载五秒后开始执行
+        var timer = null;
+        timer = setInterval(function() {
+            slider();
+        }, 5000);
+
+        //鼠标划入图片区控制计时器
+
+        $("#slider img").mouseover(function() {
+            clearInterval(timer);
+        });
+
+        //鼠标移出开启定时器，按钮小时
+        $("#slider img").mouseout(function() {
+            timer = setInterval(function() {
+                slider();
+            }, 5000)
+        });
+        $(".toggle-btn-r").click(function() {
+            slider();
+        });
+        $(".toggle-btn-l").click(function() {
+            num -= 2;
+            slider();
+        });
+
+        function slider() {
+            num++;
+            //临界值控制
+            if (num == 3) {
+                num = 0;
+            }
+            if (num == -1) {
+                num = 2;
+            }
+            // 图片切换
+            $("#slider img").eq(num).stop().animate({
+                "opacity": 1
+            }, 500);
+            $("#slider img").eq(num).siblings().stop().animate({
+                "opacity": 0
+            }, 500);
+            $(".slider-num span").eq(num).addClass("num-white").siblings().removeClass("num-white");
+        }
+    })
+    //首页顶部的效果
+$(function() {
+    //下拉菜单
+    $(".drop").mouseenter(function() {
+        $(this).css({
+            "background": "#ffffff",
+            "border-bottom": "solid 1px #ffffff"
+        })
+        $(this).children(".icon-arrow").toggleClass("icon-arrow-top")
+
+    })
+    $(".drop").mouseleave(function() {
+        $(this).css({
+            "background": "#f5f5f5",
+            "border": "none",
+        })
+        $(this).children(".icon-arrow").toggleClass("icon-arrow-top")
+    })
 })
